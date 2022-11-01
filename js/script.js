1f65768d7124add73111543502108b604ccf1374
Andy Cavedal <hcavedal@gmail.com>
2022-10-31 21:17:02 -0300

    Agregados constantes para los objetos del html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-console.log('Hello, world')
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const parrafito = document.querySelector('.parrafito');
+const pid = document.querySelector('#pid');
+const input = document.querySelector('input');
