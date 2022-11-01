657be160492e8176dee02d7c7f8db41fdfc7381e
Andy Cavedal <hcavedal@gmail.com>
2022-10-31 22:38:33 -0300

    calculadora creada con js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const parrafito = document.querySelector('.parrafito');
-const pid = document.querySelector('#pid');
-const input = document.querySelector('input');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const resultado = document.querySelector('#resultado');
+
+function btnOnClick() {
+    const sumaInputs = input1.value + input2.value;
+    resultado.innerText = "Resultado: " + sumaInputs;
+}
